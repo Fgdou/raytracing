4343a6c1ec1457eeb8f7a881e22b1f7df9baa479
fgdou <fabigoardou@gmail.com>
2023-12-01 13:30:42 +0100

    fix warning

--- src/materials/color.rs
+++ src/materials/color.rs
@@ -6,7 +6,7 @@ pub struct NormalColor {
 }
 
 impl Material for NormalColor {
-    fn get_color(&self, ray: &Ray, normal: &Ray, scene: &Scene, bounce: i32) -> RGB {
+    fn get_color(&self, _: &Ray, normal: &Ray, _: &Scene, _: i32) -> RGB {
         RGB{
             r: (normal.dir.x*255.0) as u8,
             g: (normal.dir.y*255.0) as u8,
@@ -19,7 +19,7 @@ pub struct Color {
     pub rgb: RGB
 }
 impl Material for Color {
-    fn get_color(&self, ray: &Ray, normal: &Ray, scene: &Scene, bounce: i32) -> RGB {
+    fn get_color(&self, _: &Ray, _: &Ray, _: &Scene, _: i32) -> RGB {
         self.rgb.clone()
     }
 }
